d1ae17343deb5ece839b8f667d28a4ff1601304f
Mahasak Pijittum <mahasak@gmail.com>
2017-06-23 21:31:44 +0700

    remove logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import firebase from 'firebase';
 import { Auth, Data } from './components/FirebaseReact';
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -46,7 +45,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h2>Welcome to React</h2>
         </div>
         <Auth>{this.renderApp}</Auth>
